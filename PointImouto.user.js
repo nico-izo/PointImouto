a32d9af3ae07ba85ce8d4ab4b235665966ded1da
Nicolay Izoderov <nico-izo@ya.ru>
2015-07-06 22:14:36 +0300

    Codestyle fix

--- PointImouto.user.js
+++ PointImouto.user.js
@@ -27,36 +27,36 @@
 \
 #left-menu [class*='fa-']:hover::before { opacity: 0.6; }\
 ";
-	
+
 	// this is only for debug purposes only
 	//$("head").append('<link rel="stylesheet" href="http://wtf.nico-izo.moe/pointimouto/PointImouto.css" type="text/css" />');
-	
-	
+
+
 	var avatarUrl = "//point.im/avatar/%nickname%/40";
-	
+
 	var me = $("div#name h1").html();
 	var avatar = avatarUrl.replace("%nickname%", me);
-	
+
 	metaStyle += "body #main-wrap #main #left-menu #menu-blog { background: url(" + avatar + ") no-repeat scroll 11px center; }";
-	
+
 	$("head").append("<style>" + metaStyle + "</style>");
-	
+
 	var menuLogout = $("#menu-logout");
 	var menuProfile = $("#menu-profile");
-	var menuSearch = $('<a class="fa-search" id="menu-search" href="#search"></a>');
-	
-	var menuAddPost = $('<a class="fa-plus-circle" id="add-message" href="#search"></a>');
-	
+	var menuSearch = $('<a class="fa-search" id="menu-search" href="/search"></a>');
+
+	var menuAddPost = $('<a class="fa-plus-circle" id="add-message" href="#addPost"></a>');
+
 	menuProfile.addClass("fa-wrench");
 	menuLogout.addClass("fa-sign-out")
-	
+
 	var menu = $("#left-menu");
 	var menuLast = $("#left-menu #menu-bookmarks");
-	
+
 	menuAddPost.click(function() {
 		$("#new-post-label").click();
 	});
-	
+
 	menuLast.after(menuLogout);
 	menuLast.after(menuProfile);
 	menuLast.after(menuSearch);
@@ -78,27 +78,8 @@
 			o.attr("src", o.attr("data-url"));
 			$(this).addClass("gif");
 		}
-		
+
 		return false;
 	});
-	
-	//$("#left-menu #menu-blog").attr("style", "background: url(" + avatar + ") no-repeat scroll 11px center;");
-	
+
 })(window.$);
-
-/* <form class="search-form" action="http://point.im/search">
-
-    <input type="submit" value=""></input>
-    <div>
-        <input type="text" placeholder="Поиск" value="" name="text"></input>
-    </div>
-    <label id="search-hide-label" for="search-cb"></label>
-
-</form>
-
-// @require https://github.com/radjah/pointext/raw/master/jq/jquery.js
-// @require https://github.com/radjah/pointext/raw/master/jq/bquery_ajax.js
-
-<a id="menu-logout" href="http://nico-izo.point.im/logout?csrf_token=cc556b27a87ccde1e70d1c6a5dcb791c898cb53d">Выйти</a>
-<a id="menu-profile" href="http://nico-izo.point.im/profile">Профиль</a>
-*/
